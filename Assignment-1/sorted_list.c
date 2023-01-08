331c6a4391ced029c98ffab457b4fc44281e106d
Amit Kumar <amimalik@cisco.com>
2023-01-08 09:51:46 +0530

    saving the last changes

--- Assignment-1/sorted_list.c
+++ Assignment-1/sorted_list.c
@@ -8,7 +8,7 @@
  	- RAJASEKHARUNI KRISHNA ARUN
 ----------------------------------------------------------------------------
  File Name: sorted_list.c
-    Sortel list implemenation
+    Sorted List Implimentation functional module
      --> while loop to collect the user inputs
 
  Input: cmd line inputs from user
@@ -17,8 +17,60 @@
 */
 # include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
+// Structure to create a node with data and the next pointer
+struct node {
+    int data;
+    struct node * next;
+};
+
+struct node * front = NULL;
+struct node * rear = NULL;
+
+// EnqueueLL() operation on a queue
+void enqueueLL(int value) {
+    struct node * ptr;
+    ptr = (struct node * ) malloc(sizeof(struct node));
+    ptr -> data = value;
+    ptr -> next = NULL;
+    if ((front == NULL) && (rear == NULL)) {
+        front = rear = ptr;
+    } else {
+        rear -> next = ptr;
+        rear = ptr;
+    }
+    printf("Node is Inserted\n\n");
+}
+
+// dequeueLL() operation on a queue
+int dequeueLL() {
+    if (front == NULL) {
+        printf("\nUnderflow\n");
+        return -1;
+    } else {
+        struct node * temp = front;
+        int temp_data = front -> data;
+        front = front -> next;
+        free(temp);
+        return temp_data;
+    }
+}
+
+// displayLL all elements of the queue
+void displayLL() {
+    struct node * temp;
+    if ((front == NULL) && (rear == NULL)) {
+        printf("\nQueue is Empty\n");
+    } else {
+        printf("The queue is \n");
+        temp = front;
+        while (temp) {
+            printf("%d--->", temp -> data);
+            temp = temp -> next;
+        }
+        printf("NULL\n\n");
+    }
+}
 
 /* 
  * Function name: getChoice
@@ -27,19 +79,25 @@
  * return: integer choice
  */ 
 
-int SortedLLImpChoice ( void ) {
+int getLLImpChoice ( void ) {
 	int choice;
     /*
-	printf("\t1. Input into Array Queue from File (Multiple elements)\n");
-	printf("\t2. Input into Array Queue from command line (Single Element)\n");
-	printf("\t3. Remove element from Array Queue (Single Element)\n");
+ 	printf("\t1. Input into List Queue from File\n");
+	printf("\t2. Input into List Queue from command line\n");
+	printf("\t3. Remove element from List Queue\n");
+	printf("\t4. Input into Sorted List from File\n");
+	printf("\t5. Input into Sorted List from command line\n");
+	printf("\t6. Find element in Sorted List\n");
+	printf("\t7. Remove element in Sorted List\n");    
+	printf("\t8. Press 0 to return main menu\n");	
     */
-	printf("\t1. add single element\n");
+    printf("New menu list-----------\n");
+ 	printf("\t1. add single element\n");
 	printf("\t2. add elements using file\n");
 	printf("\t3. delete element\n");
-	printf("\t4. display\n");
+	printf("\t4. displayLL\n");
 	printf("\t5. Press 0 to return main menu\n");	
-    
+ 
     // read back user inputs
 	printf("\nSelect an option: ");
 	scanf("%d", &choice);
@@ -53,38 +111,42 @@ int SortedLLImpChoice ( void ) {
  * return: None
  */ 
  
-void SortedLLImpMain (void) {
+void LLImpMain (void) {
 	int choice;
-    int value;
-    
+	int value;
+
 	do {
-   	
-        choice = SortedLLImpChoice();
+		choice = getLLImpChoice();
+		
 		switch(choice) {
 			case 1:
                 printf("\nEnter the value to insert: ");
                 scanf("%d", & value);
+                enqueueLL(value);
                 break;
 			case 2: 
-                printf("\nEnter the filename to insert elements: TBD");
-                break;
-			case 3:
-                printf("Delete an element is :%d\n", dequeue(queue));
-                break;
-			case 4:
-				printf("\ndisplay elements of queue:\n");
+			case 3: 
+                value = dequeueLL();
+                printf("popped value: %d\n", value);
+				break;
+			case 4: 
+				displayLL();
+				break;
+			case d: 
+			caiec6: 
+			case 7: 
+				printf("choice entered: %d\n", choice);
 				break;
 
-			case 0:
+			case 0: 
                 printf("moving back to main menu\n");
 				break;
 
-			default:
+			default: 
 				printf("Please enter a valid choice\n");
 				break;
 		}
 		printf("\n");
 
 	} while(choice != 0);
-
 }
